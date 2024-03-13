bb61c88a0549c33c7b4ca9ad48b636cab71cb127
moonmoonbird <lomoonmoonbird@gmail.com>
2024-03-13 14:43:58 +0800

    Update osexp.go

--- exec/model/osexp.go
+++ exec/model/osexp.go
@@ -17,14 +17,14 @@
 package model
 
 import (
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/cpu"
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/disk"
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/file"
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/mem"
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/network"
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/process"
-	"github.com/chaosblade-io/chaosblade-exec-os/exec/script"
-	"github.com/chaosblade-io/chaosblade-spec-go/spec"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/cpu"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/disk"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/file"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/mem"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/network"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/process"
+	"github.com/lomoonmoonbird/chaosblade-exec-os/exec/script"
+	"github.com/lomoonmoonbird/chaosblade-spec-go/spec"
 )
 
 type OSSubResourceModelSpec struct {
